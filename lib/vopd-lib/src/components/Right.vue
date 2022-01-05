46e9a2883c9ff306e46e16f15d974d22a9c71b2d
jzx <945627077@qq.com>
2022-01-05 16:14:48 +0800

    '更新查询构造区'

--- lib/vopd-lib/src/components/Right.vue
+++ lib/vopd-lib/src/components/Right.vue
@@ -1,12 +1,16 @@
 <template>
-    <div class="w-[40rem] flex">
+    <div class="w-[60rem] flex">
         <div class="border  w-[20rem]">
             <MultipleSelectView class="border-t " />
-            <DataView class="border-t "/>
+            
         </div>
         <div class="border  w-[20rem]">
             <TreeView class="border-t "/>
             <AttrView class="border-t " />
+        </div>
+         <div class="border  w-[20rem]">
+            <DataView class="border-t "/>
+            <QueryView class="border-t "/>
         </div>
     </div>
 </template>
@@ -17,9 +21,11 @@ import TreeView from "../view/TreeView/TreeView.vue"
 import AttrView from "../view/AttrView/AttrView.vue"
 
 import DataView from "../view/DataView/DataView.vue"
+import QueryView from "../view/DataView/QueryView.vue"
+
 import MultipleSelectView from "../view/MultipleSelectView/MultipleSelectView.vue"
 export default defineComponent({
-  components:{TreeView,AttrView,DataView,MultipleSelectView},
+  components:{TreeView,AttrView,DataView,MultipleSelectView,QueryView},
   setup() {
     
   }
