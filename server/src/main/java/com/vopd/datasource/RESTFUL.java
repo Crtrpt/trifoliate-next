f5ef814cfeae9655ecbf03f12fadf7b54260373c
jzx <945627077@qq.com>
2022-01-07 11:38:46 +0800

    'updateExampleServer'

--- server/src/main/java/com/vopd/datasource/RESTFUL.java
+++ server/src/main/java/com/vopd/datasource/RESTFUL.java
@@ -4,18 +4,25 @@ import com.google.gson.internal.LinkedTreeMap;
 import com.vopd.dto.ApFlat;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.*;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -23,7 +30,7 @@ public class RESTFUL {
 
     static HashMap<String, PoolingHttpClientConnectionManager> sourcePools = new HashMap<>();
 
-    public Object run(ApFlat ap) throws IOException {
+    public Object run(ApFlat ap) throws IOException, URISyntaxException {
         var connectionManager = sourcePools.get(ap.getSource().getId());
         //2.创建httpclient对象
         var httpClient = HttpClients.custom()
@@ -34,7 +41,18 @@ public class RESTFUL {
         var query = (LinkedTreeMap<String, Object>) ap.getAp().getQuery();
 
         log.info(" url ===========" + url + (String) query.get("path"));
-        HttpGet httpGet = new HttpGet(url + (String) query.get("path"));
+
+
+
+        HttpEntityEnclosingRequestBase httpRequestBase = new HttpEntityEnclosingRequestBase ()  {
+            @Override
+            public String getMethod() {
+                return (String) query.get("method");
+            }
+        };
+        httpRequestBase.setEntity(new StringEntity((String) query.get("requestBody"), Charset.defaultCharset()));
+        httpRequestBase.setURI(new URI(url + (String) query.get("path")));
+
         //设置请求参数
         RequestConfig config = RequestConfig.custom().setConnectTimeout(5000) //连接超时时间
                 .setConnectionRequestTimeout(500) //从线程池中获取线程超时时间
@@ -42,13 +60,17 @@ public class RESTFUL {
                 .setStaleConnectionCheckEnabled(true) //提交请求前检查连接是否可用
                 .build();
 
-        httpGet.setConfig(config);
+        httpRequestBase.setConfig(config);
+
+        ((List<Map>) query.get("header")).stream().forEach(a->{
+            httpRequestBase.addHeader(new BasicHeader((String) a.get("name"),(String)a.get("value")));
+        });
 
         //返回数据
         CloseableHttpResponse response = null;
         String con = "";
         try {
-            response = httpClient.execute(httpGet);
+            response = httpClient.execute(httpRequestBase);
             con = EntityUtils.toString(response.getEntity(), "utf-8");
 //            System.out.println(con);
         } catch (ClientProtocolException e) {
@@ -73,9 +95,6 @@ public class RESTFUL {
                 TimeUnit.MILLISECONDS);
         connectionManager.setMaxTotal(1000);//1.2
         connectionManager.setDefaultMaxPerRoute(50);//1.3
-
-
         sourcePools.put(ap.getSource().getId(), connectionManager);
-
     }
 }
--- server/src/main/java/com/vopd/server/ExampleHttpServer.java
+++ server/src/main/java/com/vopd/server/ExampleHttpServer.java
@@ -11,6 +11,7 @@ import com.vopd.entity.Ap;
 import io.undertow.Undertow;
 import io.undertow.util.Headers;
 import lombok.extern.slf4j.Slf4j;
+import org.xnio.streams.ChannelInputStream;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,14 +22,24 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
-public class JsonServer {
+public class ExampleHttpServer {
     public static void main(String[] argv) throws SQLException, FileNotFoundException {
         Gson gson=new Gson();
         Undertow server = Undertow.builder()
                 .addHttpListener(8081, "localhost")
                 .setHandler(exchange -> {
                     log.info("url:  "+exchange.getRequestURL());
-                    var data=gson.toJson(Map.of("name","server","time",System.currentTimeMillis()));
+
+
+                    var s=new ChannelInputStream(exchange.getRequestChannel());
+                    var body=s.readAllBytes();
+                    var data=gson.toJson(Map.of(
+                            "url:  ",exchange.getRequestURL(),
+                            "method",exchange.getRequestMethod().toString(),
+                            "header",exchange.getRequestHeaders().toString(),
+                            "name","server",
+                            "body",new String(body),
+                            "time",System.currentTimeMillis()));
                     exchange.getResponseSender().send(data);
                 }).build();
         log.info("start server");
