1470bd266c22d14611bcfc25630c46451a099bb4
jzx <945627077@qq.com>
2022-03-08 18:50:18 +0800

    '更新'

--- lib/vopd-lib/src/components/Center.vue
+++ lib/vopd-lib/src/components/Center.vue
@@ -1,7 +1,20 @@
 <template>
-    <div   :class="[modeList[this.mode].cursor]"  ref="view"  >
+      <Control class=" left-8 mt-4  top-4 sticky z-20"  >
+              <div class="ToolView border border-t-gray-200 p-1 absolute">
+                  <div class="item">
+                    <i class="las la-mouse-pointer" @click="changeMode(0)"></i>
+                  </div>
+                  <div class="item">
+                    <i class="las la-hand-paper" @click="changeMode(1)"></i>
+                  </div>
+                  <div class="item">
+                    <i class="las la-plug" @click="changeMode(2)"></i>
+                  </div>
+              </div>
+      </Control>
+    <div   class="z-10" :class="[modeList[this.mode].cursor]"  ref="view"  >
      
-      <div ref="canvas" class="  relative overflow-hidden  border shadow " :style="{width:'1024px',height:'960px'}">
+      <div ref="canvas" class="  relative overflow-hidden  border shadow doc " :style="{width:'1024px',height:'960px'}">
         <div class="layers">
          <Layer name="refLineLayer" />
         
@@ -15,8 +28,44 @@
      
         </div>
       </div>
-       
     </div>
+    
+            <!-- <Control class="top-0  left-0   w-full   sticky  bg-white">
+                <i class="las la-eye w-6 h-6"></i>
+            </Control>
+            <Control class="top-0    w-full flex  absolute  bg-white">
+                <div class="h-6 border shadow flex-grow"></div>
+            </Control>
+            <Control class="top-0  h-full flex text-center  sticky bg-white">
+                <div class=" w-6 border shadow flex-grow"></div>
+            </Control>
+             -->
+            <Control class=" right-5 mt-4 top-4  sticky ">
+              <div class=" ">
+                 <div class="flex items-center justify-center border rounded-full mx-1 px-2 bg-gray-200 shadow">
+                <input class=" inline text-sm w-10 text-center bg-inherit" value="1024"/>
+                <p class="text-sm">/</p>
+                <input class=" inline text-sm w-10 text-center bg-inherit" value="960"/>
+              </div>
+              </div>
+              
+            </Control>
+
+             <!-- <Control class=" right-5 top-6  sticky ">
+              <div class=" absolute">
+                
+              </div>
+            </Control> -->
+           
+            <Control class=" left-5 mt-4 top-4 sticky ">
+               <div class=" ">
+                <div class="flex items-center justify-center border rounded-full bg-gray-200 shadow">
+                  <i class=" item las la-search-plus leading-3 px-1 "></i>
+                  <input class=" inline text-sm w-10 bg-inherit" value="100%"/>
+                  <i class="item las la-search-minus leading-3 px-1 "></i>
+                </div>
+                </div>
+            </Control> 
 </template>
 
 <script lang="ts">
