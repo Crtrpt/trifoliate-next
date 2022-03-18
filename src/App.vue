1aa83aa0d6a7e579df870ff391381387c581fd66
jzx <945627077@qq.com>
2022-03-18 15:05:20 +0800

    '修改预览图'

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,140 @@ export default {
     return {
       nodeList:[
                 {
-                    id:"1",
-                    name:"容器",
+                  name:"固定",
+                  list:[
+                    {
+                    id:"100",
+                    name:"固定布局",
                     render:"ContainerRender",
                     widget:"Container",
                     icon:"las la-stop",
                     style:{
+                      height:"200px"
+                    },
+                    children:[]
+                },
+                {
+                    id:"110",
+                    name:"浮动布局",
+                    render:"ContainerRender",
+                    widget:"Container",
+                    icon:"las la-stop",
+                    style:{
+                      position:"absolute",
+                      left:"100px",
+                      top:"100px",
                       width:"400px",
                       height:"200px",
                     },
                     children:[]
                 },
                 {
+                    id:"101",
+                    name:"行布局",
+                    render:"ContainerRender",
+                    widget:"Container",
+                    icon:"las la-stop",
+                    style:{
+                      display:"flex",
+                    },
+                    children:[]
+                },
+                 {
+                    id:"111",
+                    name:"列布局",
+                    render:"ContainerRender",
+                    widget:"Container",
+                    icon:"las la-columns",
+                    style:{
+                      flexDirection: "column",
+                      display:"flex",
+                    },
+                    children:[]
+                },
+                {
+                    id:"102",
+                    name:"1/2宽布局",
+                    render:"ContainerRender",
+                    widget:"Container",
+                    icon:"las la-stop",
+                    style:{
+                      flexDirection: "column",
+                      display:"flex",
+                      width:"50%",
+                    },
+                    children:[]
+                },
+                {
+                    id:"103",
+                    name:"1/3宽布局",
+                    render:"ContainerRender",
+                    widget:"Container",
+                    icon:"las la-stop",
+                    style:{
+                      flexDirection: "column",
+                      display:"flex",
+                     width:"33.33%",
+                    },
+                    children:[]
+                },
+                {
+                    id:"104",
+                    name:"1/4宽布局",
+                    render:"ContainerRender",
+                    widget:"Container",
+                    icon:"las la-stop",
+                    style:{
+                      flexDirection: "column",
+                      display:"flex",
+                      width:"25%",
+                    },
+                    children:[]
+                },
+                {
+                    id:"105",
+                    name:"1/5宽布局",
+                    render:"ContainerRender",
+                    widget:"Container",
+                    icon:"las la-stop",
+                    style:{
+                      flexDirection: "column",
+                      display:"flex",
+                      width:"20%",
+                    },
+                    children:[]
+                },
+                {
+                    id:"106",
+                    name:"剩余空间布局",
+                    render:"ContainerRender",
+                    widget:"Container",
+                    icon:"las la-stop",
+                    style:{
+                     
+                      display:"flex",
+                      flexGrow:"1",
+                    },
+                    children:[]
+                },
+                  {
+                          id:"201",
+                          name:"表单布局",
+                          render:"FormRender",
+                          widget:"Form",
+                          icon:"las la-stop",
+                          style:{
+                            width:"100%"
+                          },
+                          children:[]
+                      },
+                
+                ]
+                },
+                {
+                  name:"元素",
+                  list:[
+                    {
                     id:"5",
                     name:"文字",
                     widget:"Text",
@@ -72,6 +194,41 @@ export default {
                       "width":"100%",
                       "height":"100%",
                     }
+                }
+                  ],
+                },
+                {
+                  name:"表单",
+                  list:[
+                      {
+                          id:"202",
+                          name:"输入框",
+                          render:"InputRender",
+                          widget:"Input",
+                          icon:"las la-stop",
+                          meta:{
+                            type:"text",
+                          },
+                          style:{
+
+                          },
+                          children:[]
+                      },
+                      {
+                          id:"203",
+                          name:"选择框",
+                          render:"InputRender",
+                          widget:"Input",
+                          icon:"las la-stop",
+                          meta:{
+                            type:"checkbox",
+                          },
+                          style:{
+
+                          },
+                          children:[]
+                      }
+                  ]
                 }
       ],
       data:{
