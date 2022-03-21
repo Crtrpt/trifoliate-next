6c5a4faa52c7091b862c843663eefdc1d60b6cd0
jzx <945627077@qq.com>
2022-03-21 10:31:21 +0800

    '增加用户空间概念方便用户扩展'

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import App from './App.vue'
 import VopdLib from '../lib/vopd-lib/src/VopdLib'
 
 
+
+import FormRender from "./userDefine/FormRender.vue";
+import InputRender from "./userDefine/InputRender.vue"
+
 let app=createApp(App);
 
-app.use(VopdLib);
-
+app.use(VopdLib,{
+  render:[
+    FormRender,InputRender
+  ]
+});
 
 app.mount('#app')
---
+++ src/userDefine/InputRender.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    :style="data.style"
+    ref="node"
+    :type="data.meta.type"
+    v-model="data.value"
+  />
+</template>
+
+<script lang="ts">
+export default {
+  components: {},
+  props: {
+    data: Object,
+  },
+  methods: {},
+  name: "InputRender",
+  mounted() {},
+};
+</script>
