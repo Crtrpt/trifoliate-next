1b69e2287eb6a594ba51eff93a857a5fb11f852e
jzx <945627077@qq.com>
2022-03-22 19:12:22 +0800

    '修改wvm版本'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-      "./lib/vopd-lib/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./lib/vopd-lib/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_nodules/vopd-wvm/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
