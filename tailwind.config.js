3ef70d569821a0a7a718ac93bc1a435a2579d66e
jzx <945627077@qq.com>
2022-03-22 20:46:05 +0800

    '删除重复的taiwincss'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  mode: "jit",
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./lib/vopd-lib/**/*.{vue,js,ts,jsx,tsx}"
+    "./lib/vopd-lib/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/vopd-wvm/src/components/*.vue",
   ],
   theme: {
     extend: {},
